be040cb9bdc4dfb3020ecedc11a3f21c9153b414
Pedro Vidal <up202108754@up.pt>
2026-03-28 20:41:45 +0000

    feat: Durable Object Websocket echo

--- backend/src/do/Session.ts
+++ backend/src/do/Session.ts
@@ -11,13 +11,47 @@ export class Session extends DurableObject<Env> {
 
     }
 
-async sayHello(): Promise <string> {
+    async fetch(request: Request): Promise<Response> {
 
-    let result = this.ctx.storage.sql
-        .exec('SELECT "Hello, World, from DO!" AS message')
-        .one();
+        if (request.headers.get('Upgrade') === 'websocket') {
+            return this.handleWebSocket(request);
+        }
+        // Handle other HTTP requests here
+        return new Response('Invalid request', { status: 400 });
+    }
 
 
-    return result.message as string;
-}
+    async handleWebSocket(request: Request): Promise<Response> {
+
+        const  webSocketPair = new WebSocketPair();
+        const [client, server] = Object.values(webSocketPair);
+        this.ctx.acceptWebSocket(server);
+     
+        return new Response(null, {
+            status: 101,
+            webSocket: client,
+        });
+    }
+
+    async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string ) {
+
+        ws.send(`[DO] message: ${message}`);
+
+    }
+
+    async webSocketClose(ws: WebSocket, code: number, reason: string) {
+    
+        ws.close(code,reason);
+
+    }
+
+    async sayHello(): Promise <string> {
+
+        let result = this.ctx.storage.sql
+            .exec('SELECT "Hello, World, from DO!" AS message')
+            .one();
+
+
+        return result.message as string;
+    }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,6 +18,18 @@ app.get('/hello', async (c) => {
   return c.text(message);
 });
 
+app.get('/ws', async (c) => {
+
+		const id = c.env.SESSION.idFromName('SESSION');
+		const stub = c.env.SESSION.get(id);
+		const response = await stub.fetch(c.req.raw);
+
+		return response;
+
+	}
+
+)
+
 export default app;
 export { Session } from './do/Session';
 
